678a9b8a380c26e16da7a6cf42d53aeec1ca1756
simnmaier <MAierbuagreen95!>
2019-04-13 17:01:30 +0200

    fixed addRennschnecke method

--- src/Rennen.java
+++ src/Rennen.java
@@ -2,11 +2,14 @@ public class Rennen extends Rennschnecke {
     private String name;
     private int teilnehmer;
     private  Rennschnecke [] rennschnecken;
+    private int zielstrecke;
 
-    public Rennen (String name, int teilnehmer){
+    public Rennen (String name, int teilnehmer, int zielstrecke){
         this.name = name;
         teilnehmer = 1;
         this.teilnehmer = teilnehmer;
+        this.rennschnecken = new Rennschnecke[]{};
+        this.zielstrecke = zielstrecke;
     }
 
     public void addRennschnecke (Rennschnecke neueSchnecke){
@@ -26,8 +29,12 @@ public class Rennen extends Rennschnecke {
         return "Das Rennen mit dem Namen " + this.name + " hat " + this.rennschnecken.length + " Teilnehmer" + "\n";
     }
 
+    public Rennschnecke ermittleGewinner(){
+        if ()
+    }
+
     public static void main (String [] args){
-        Rennen ty = new Rennen("Racing", 3);
+        Rennen ty = new Rennen("Racing", 3,30);
         Rennschnecke gerry = new Rennschnecke("Gerry", "Tigerschnecke", 23, 12){};
 
         ty.addRennschnecke(gerry);
